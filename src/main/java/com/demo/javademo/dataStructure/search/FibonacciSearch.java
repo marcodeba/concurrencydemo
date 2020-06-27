b2d6f71b4931fe2c8770373cd812fc9f52b88bcd
panning255 <panning255@jk.cn>
2020-06-27 23:30:20 +0800

    add sequence code

--- src/main/java/com/demo/javademo/dataStructure/search/FibonacciSearch.java
+++ src/main/java/com/demo/javademo/dataStructure/search/FibonacciSearch.java
@@ -8,9 +8,9 @@ import java.util.Arrays;
  *  
  * 算法复杂度 O(logn)
  *
- * 二分查找分割方式 mid = (low+high)/2  加法和除法
- * 插值查找分割方式 mid = low + (high-low)*(key-a[low])/(a[high]-a[low]) 加减乘除都用上了
- * 斐波那契查找分割方式 mid = low + Fibonacci[index - 1] - 1
+ * 二分查找分割方式 middle = (low+high)/2  加法和除法
+ * 插值查找分割方式 middle = low + (high-low)*(key-a[low])/(a[high]-a[low]) 加减乘除都用上了
+ * 斐波那契查找分割方式 middle = low + Fibonacci[index - 1] - 1
  *
  * Fibonacci Search examines relatively closer elements in subsequent steps.
  * So when input array is big that cannot fit in CPU cache or even in RAM, Fibonacci Search can be useful.
@@ -19,8 +19,7 @@ public class FibonacciSearch {
     public static void main(String[] args) {
         int[] a = new int[]{ 1, 5, 15, 22, 25, 31, 39, 42, 47, 49, 59, 68, 88, 88};
         System.out.println("待查找数组 a：" + Arrays.toString(a));
-
-        //查找示例1
+        System.out.println("待查找数组长度为：" + a.length);
         int key = 59;
         System.out.println("寻找的key为：" + key);
         System.out.println("结果在数组 a 角标的 [" + fibonacciSearch(key, a) + "] 位");
@@ -43,10 +42,7 @@ public class FibonacciSearch {
         index -= 1;
 
         //用于展示 
-        System.out.println("待查找数组长度为：" + a.length);
         System.out.println("斐波那契数列为：" + fibonacciArray.toString());
-        System.out.println("斐波那契角标 [" + (index - 2) + "] 位为：" + getFibonacci(index - 2));
-        System.out.println("斐波那契角标 [" + (index - 1) + "] 位为：" + getFibonacci(index - 1));
         System.out.println();
 
         //定义临时数组来扩展待查数组的长度，长度为 fibo 
@@ -61,24 +57,24 @@ public class FibonacciSearch {
         System.out.println("补充后的数组 temp 为：" + Arrays.toString(temp));
         System.out.println("补充后的数组长度为：" + temp.length);
 
-        //初始化记录首位 末位 mid非字面意义上的中间值，仅是将数组分割为两部分
+        //初始化记录首位 末位 middle非字面意义上的中间值，仅是将数组分割为两部分
         int low = 0;
         int high = a.length - 1;
-        int mid;
+        int middle;
         while (low <= high) {
             //计算分割数组处的角标
-            mid = low + fibonacciArray.get(index - 1) - 1;
-            System.out.println("斐波那契角标 =  " + index + "   将key与数组的角标 [" + (mid) + "] 所在的值做比较");
-            if (key < temp[mid]) {
-                high = mid - 1;
+            middle = low + fibonacciArray.get(index - 1) - 1;
+            System.out.println("斐波那契角标 =  " + index + "   将key与数组的角标 [" + (middle) + "] 所在的值做比较");
+            if (key < temp[middle]) {
+                high = middle - 1;
                 index -= 1;
-            } else if (key > temp[mid]) {
-                low = mid + 1;
+            } else if (key > temp[middle]) {
+                low = middle + 1;
                 index -= 2;
             } else {
-                //如果值相等且角标小于或等于待查数组的最大角标 返回mid 表示找到 
-                if (mid <= a.length - 1) {
-                    return mid;
+                //如果值相等且角标小于或等于待查数组的最大角标 返回middle 表示找到 
+                if (middle <= a.length - 1) {
+                    return middle;
                 } else {
                     //如果值相等但角标大于待查数组的最大角标 
                     //这样表示在与temp数组比较时，比较的角标超过了待查数组的角标 
