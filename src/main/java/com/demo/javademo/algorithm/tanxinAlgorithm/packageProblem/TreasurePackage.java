200e309e2135a6c749fcb8c4dea2aff6d3f2dba2
panning255 <panning255@jk.cn>
2020-09-09 16:11:10 +0800

    增加贪心算法下的阿里巴巴与四十大盗的背包算法

--- src/main/java/com/demo/javademo/algorithm/tanxinAlgorithm/packageProblem/TreasurePackage.java
+++ src/main/java/com/demo/javademo/algorithm/tanxinAlgorithm/packageProblem/TreasurePackage.java
@@ -3,34 +3,43 @@ package com.demo.javademo.algorithm.tanxinAlgorithm.packageProblem;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 阿里巴巴与四十大盗背包问题
+ */
 public class TreasurePackage {
     public static void main(String[] args) {
         System.out.print("请输入毛驴的承载能力：");
         Scanner input = new Scanner(System.in);
-        double capacity = input.nextDouble();
+        // 背包最大容量
+        double maxCapacity = input.nextDouble();
+        // 每件宝物重量，价值和单位重量价值
         Treasure[] treasures = initializData();
+        // 根据单位重量价值做排序
         Arrays.sort(treasures);
 
+        // 装入宝箱的宝物的价值
         double sumValue = 0.0;
-        for (int i = 0; i < treasures.length; i++) {
-            if (capacity > treasures[i].getWeight()) {
-                capacity -= treasures[i].getWeight();
-                sumValue += treasures[i].getValue();
+        for (Treasure treasure : treasures) {
+            // 背包装得下
+            if (maxCapacity > treasure.getWeight()) {
+                maxCapacity -= treasure.getWeight();
+                sumValue += treasure.getValue();
             } else {
-                sumValue += capacity * treasures[i].getUnitValue();
+                // 背包装不下完整宝物，将宝物拆分
+                sumValue += maxCapacity * treasure.getUnitValue();
                 break;
             }
         }
-        System.out.println("装入宝箱的最大价值是:" + sumValue);
+        System.out.println("装入宝箱的宝物最大总价值是:" + sumValue);
     }
 
-    public static Treasure[] initializData() {
+    private static Treasure[] initializData() {
         Scanner input = new Scanner(System.in);
         System.out.print("请输入宝物数量：");
         int treasureCount = input.nextInt();
 
         Treasure[] treasures = new Treasure[treasureCount];
-        System.out.println("请输入每个宝物的重量和价值，用空格分开：");
+        System.out.println("请输入每个宝物的重量和价值：");
         for (int i = 0; i < treasureCount; i++) {
             System.out.println("请第" + (i + 1) + "个宝物的重量");
             double weight = input.nextDouble();
@@ -43,9 +52,13 @@ public class TreasurePackage {
     }
 }
 
+// 宝物对象
 class Treasure implements Comparable<Treasure> {
+    // 重量
     private double weight;
+    // 价值
     private double value;
+    // 单位重量价值
     private double unitValue;
 
     public double getValue() {
@@ -57,7 +70,7 @@ class Treasure implements Comparable<Treasure> {
     }
 
     public double getUnitValue() {
-        return this.getValue() / this.getWeight();
+        return this.value / this.weight;
     }
 
     public Treasure(double weight, double value) {
