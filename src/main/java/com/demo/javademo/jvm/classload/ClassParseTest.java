012ede130dfe78520aefb444ab04847271156204
panning255 <panning255@jk.cn>
2021-06-29 21:08:53 +0800

    add classloader

--- src/main/java/com/demo/javademo/jvm/classload/ClassParseTest.java
+++
@@ -1,9 +0,0 @@
-package com.demo.javademo.jvm.classload;
-
-public class ClassParseTest {
-    private static final String test = "test string";
-
-    public static void main(String[] args) {
-        System.out.println(test);
-    }
-}
--- src/main/java/com/demo/javademo/jvm/classload/Test.java
+++
@@ -1,9 +0,0 @@
-package com.demo.javademo.jvm.classload;
-
-public class Test {
-    private static final int a = 10;
-
-    public static void main(String[] args) {
-        System.out.println(a);
-    }
-}
